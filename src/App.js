7dc79d40817aa7f12a261b552169db13f06cacb9
Dasha <dasha.kotova.mail@gmail.com>
2022-12-28 19:38:53 -0500

    add basic setup and pages

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+
+const App = () => {
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Dashboard/>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
