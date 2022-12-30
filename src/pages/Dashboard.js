e42610c42b39b01971ed12f2bd06816e51432986
Dasha <dasha.kotova.mail@gmail.com>
2022-12-29 20:11:41 -0500

    add dashboard display with data and styling

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,65 @@
+import TicketCard from '../components/TicketCard'
+
 const Dashboard = () => {
+
+    const tickets = [
+        {
+            category: 'Q1 2022',
+            color: 'red',
+            title: 'NFT Safety 101 Videos',
+            owner: 'Joe Mama',
+            avatar: 'http://www.henhunt.co.uk/wp-content/uploads/2014/10/Person-Logo-1.png',
+            status: 'done',
+            priority: 5,
+            progress: 40,
+            description: 'Make a video on NFTs',
+            timestamp: '2022-02-11T07:36:17+0000'
+        },
+        {
+            category: 'Q2 2022',
+            color: 'blue',
+            title: 'Build and Sell AI Model',
+            owner: 'Joe Mama',
+            avatar: 'http://www.henhunt.co.uk/wp-content/uploads/2014/10/Person-Logo-1.png',
+            status: 'working on it',
+            priority: 2,
+            progress: 70,
+            description: 'Make a video on AI',
+            timestamp: '2022-02-11T07:36:17+0000'
+        },
+    ]
+
+    const colors = [
+        'rgb(255, 179, 186)',
+        'rgb(255, 223, 186)',
+        'rgb(255, 255, 186)',
+        'rgb(186, 255, 201)',
+        'rgb(186, 255, 255)',
+    ]
+
+    const uniqueCategories = [
+        ...new Set(tickets?.map(({ category }) => category))
+    ]
+
     return (
-        <div>Dashboard</div>
+        <div className="dashboard">
+            <h1>My Projects</h1>
+            <div className="ticket-container">
+                {tickets && uniqueCategories?.map((uniqueCategory, categoryIndex) => (
+                    <div key={categoryIndex}>
+                        <h3>{uniqueCategory}</h3>
+                        {tickets.filter(ticket => ticket.category === uniqueCategory).map((filteredTicket, _index) => (
+                            <TicketCard
+                                id={_index}
+                                color={colors[categoryIndex] || colors[0]}
+                                ticket={filteredTicket}
+                            />
+                        ))
+                        }
+                    </div>
+                ))}
+            </div>
+        </div>
     )
 }
 
--- src/pages/TicketPage.js
+++ src/pages/TicketPage.js
@@ -1,6 +1,30 @@
+import { useState } from 'react';
+
 const TicketPage = () => {
+
+    const [formData, setFormData] = useState({
+        status: 'not started',
+        progress: 0,
+    })
+
+    const editMode = false;
+
+    const handleSubmit = () => {
+        console.log('submitted')
+    }
+
     return (
-        <div>TicketPage</div>
+        <div className="ticket">
+            <h1>{editMode ? 'Update your Ticket' : 'Create a Ticket'}</h1>
+            <div className="ticket-container">
+                <form onSubmit={handleSubmit}>
+                    <section>
+                        <label>Title</label>
+                        <input id="title" name="title" type="text" onChange={handleChange} value={FormData.title} />
+                    </section>
+                </form>
+            </div>
+        </div>
     )
 }
 
